04bc80a8c3c329f0a17a28b2c4af8d02ef0d4889
timliuGH <timsgithub@gmail.com>
2019-06-25 13:56:41 -0700

    Add animations to trashcan and plus sign icons

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,27 +1,14 @@
-// Check off specific todos by clicking
-$("#list").on("click", "li", function(){	// Add listener to existing ul parent, and to li's inside ul, including new ones added dynamically
-	// if ($(this).css("color") == "rgb(128, 128, 128)") {
-	// 	$(this).css({
-	// 		color: "black",
-	// 		textDecoration: "none"
-	// 	});
-
-	// }
-	// else {
-	// 	$(this).css({
-	// 		color: "gray",
-	// 		textDecoration: "line-through"
-	// 	});
-	// }
+// Strikethrough todos by clicking on them
+$("#list").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-// Click on X to delete todo
+// Delete todos by clicking on trashcan
 $("#list").on("click", "span", function(event){
 	// Fade out AND remove the li (fadeOut only changes display to none)
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
-	});	// fadeOut just makes display: none; remove() removes the element
+	});
 	event.stopPropagation();	// Handle event bubbling
 });
 
@@ -31,8 +18,12 @@ $("input[type='text']").keypress(function(event){
 		// Get todo text from input
 		let todoText = $(this).val();
 		// Create new li and add to ul
-		$("#list").append("<li><span><i class=\"far fa-trash-alt\"></i></span> " + todoText + "</li>");
+		$("#list").append("<li><span><i class='far fa-trash-alt'></i></span> " + todoText + "</li>");
 		// Clear out input
 		$(this).val("");
 	}
+});
+
+$(".fa-plus").click(function() {
+	$("#todoInput").fadeToggle();
 });
